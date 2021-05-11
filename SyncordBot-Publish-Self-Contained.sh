131285d3dc0f2e4c03d6227ef53cce9c84ed637f
AlmightyLks <AlmightyLks@protonmail.com>
2021-05-11 11:46:14 +0200

    Adjusted publishing properties / options

--- SyncordBot-Publish-Self-Contained.sh
+++ SyncordBot-Publish-Self-Contained.sh
@@ -1,2 +1,2 @@
-dotnet publish -c Release -r win-x64 --self-contained true
-dotnet publish -c Release -r linux-x64 --self-contained true
+dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true --self-contained true
+dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained true
