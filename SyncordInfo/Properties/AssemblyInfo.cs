741b22545d25d7d09018c69669a09f0b70ebd5db
AlmightyLks <AlmightyLks@protonmail.com>
2021-05-11 09:45:30 +0200

    Modernized csproj

--- SyncordInfo/Properties/AssemblyInfo.cs
+++ SyncordInfo/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SyncordInfo")]
-[assembly: AssemblyDescription("Shared Library for Syncord")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("AlmightyLks")]
-[assembly: AssemblyProduct("SyncordInfo")]
-[assembly: AssemblyCopyright("Copyright © AlmightyLks 2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -21,16 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5c5f343b-715b-446b-8c4e-d6fd34eb41eb")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.1.0")]
-[assembly: AssemblyFileVersion("0.9.1.0")]
